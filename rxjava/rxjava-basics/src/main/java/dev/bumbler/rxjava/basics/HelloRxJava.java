3a9a35ede4e0235a0b5a9478d939f1789ec8cc13
anshulgammy <anshulgammy@gmail.com>
2022-03-25 15:01:04 +0530

    RxJava basics

--- rxjava/rxjava-basics/src/main/java/dev/bumbler/rxjava/basics/HelloRxJava.java
+++ rxjava/rxjava-basics/src/main/java/dev/bumbler/rxjava/basics/HelloRxJava.java
@@ -1,9 +1,11 @@
 package dev.bumbler.rxjava.basics;
 
 import io.reactivex.Observable;
+import io.reactivex.schedulers.Schedulers;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 public class HelloRxJava {
 
@@ -11,6 +13,11 @@ public class HelloRxJava {
         observableFromJust();
         observableFromIterable();
         observableFromCreate();
+        observableFromRange();
+        observableFromRangeWithThread();
+        observableWithErrorHandlingAtSubscribeLevel();
+        observableWithErrorHandlingAtOperatorLevel();
+        observableWithRetry();
     }
 
     /**
@@ -44,7 +51,150 @@ public class HelloRxJava {
             observableEmitter.onNext(4);
             observableEmitter.onNext(5);
         });
-        observable.subscribe(HelloRxJava::print);
+
+        observable
+                // filtering the element on the basis of odd and even. Only even numbers will be considered.
+                .filter(element -> element % 2 == 0)
+                // multiplying the element with 10
+                .map(element -> element * 10)
+                .subscribe(HelloRxJava::print);
+    }
+
+    /**
+     * Creating Observable from ObservableOnSubscribe<T> using range, and subscribing to it.
+     * Also employed filter() and map() Operators.
+     */
+    private static void observableFromRange() {
+        System.out.println("observableFromRange::Example---------->");
+        Observable<Integer> observable = Observable.range(1, 10);
+
+        observable
+                // filtering the element on the basis of odd and even. Only even numbers will be considered.
+                .filter(element -> element % 2 == 0)
+                // multiplying the element with 10
+                .map(element -> element * 10)
+                .subscribe(HelloRxJava::print);
+    }
+
+    /**
+     * Demonstrating switching of threads by Scheduler
+     */
+    private static void observableFromRangeWithThread() {
+        System.out.println("observableFromRangeWithThread::Example---------->");
+
+        Observable<Integer> observable = Observable.range(1, 10);
+
+        // odd numbers will be processed on IO thread
+        observable.range(1, 10)
+                .filter(element -> element % 2 == 0)
+                .map(element -> element * 10)
+                .observeOn(Schedulers.io())
+                .subscribe(element -> {
+                    System.out.println(element);
+                    System.out.println(Thread.currentThread().getName());
+                });
+
+        // odd numbers will be processed on Computation thread
+        observable
+                .filter(element -> element % 2 == 1)
+                .observeOn(Schedulers.computation())
+                .map(element -> element * 10)
+                .subscribe(element -> {
+                    System.out.println(Thread.currentThread().getName());
+                    print(element);
+                });
+
+        // This is important to add a thread sleep below.
+        // Otherwise, the Java main thread will end as soon as the execution passes .subscribe() above.
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Demonstrating error handling at Subscribe level
+     */
+    private static void observableWithErrorHandlingAtSubscribeLevel() {
+        System.out.println("observableWithErrorHandlingAtSubscribeLevel::Example---------->");
+
+        // Observable doesn't allow to emmit null. I have added that on purpose so that Exception gets raised.
+        Observable<Integer> observable = Observable.create(emitter -> {
+            emitter.onNext(1);
+            emitter.onNext(2);
+            emitter.onNext(3);
+            emitter.onNext(4);
+            emitter.onNext(5);
+            emitter.onNext(null);
+        });
+
+        observable
+                // filtering the element on the basis of odd and even. Only even numbers will be considered.
+                .filter(element -> element % 2 == 0)
+                // multiplying the element with 10
+                .map(element -> element * 10)
+                .subscribe(System.out::println, (error) -> {
+                    System.out.println("Error happened: " + error.getMessage());
+                });
+    }
+
+    /**
+     * Demonstrating error handling at Operator level
+     */
+    private static void observableWithErrorHandlingAtOperatorLevel() {
+        System.out.println("observableWithErrorHandlingAtOperatorLevel::Example---------->");
+
+        // Observable doesn't allow to emmit null. I have added that on purpose so that Exception gets raised.
+        Observable<Integer> observable = Observable.create(emitter -> {
+            emitter.onNext(1);
+            emitter.onNext(2);
+            emitter.onNext(3);
+            emitter.onNext(4);
+            emitter.onNext(5);
+            emitter.onNext(null);
+        });
+
+        observable
+                // filtering the element on the basis of odd and even. Only even numbers will be considered.
+                .filter(element -> element % 2 == 0)
+                // multiplying the element with 10
+                .map(element -> element * 10)
+                .onErrorReturnItem(-1)
+                .subscribe(System.out::println);
+    }
+
+    /**
+     * Demonstrating error handling with retry at Subscribe level
+     */
+    private static void observableWithRetry() {
+        System.out.println("observableWithErrorHandlingAndRetryAtSubscribeLevel::Example---------->");
+
+        // Observable doesn't allow to emmit null. I have added that on purpose so that Exception gets raised.
+        Observable<Integer> observable = Observable.create(emitter -> {
+            emitter.onNext(1);
+            emitter.onNext(2);
+            emitter.onNext(3);
+            emitter.onNext(4);
+            emitter.onNext(5);
+            emitter.onNext(null);
+        });
+
+        observable
+                // filtering the element on the basis of odd and even. Only even numbers will be considered.
+                .filter(element -> element % 2 == 0)
+                // multiplying the element with 10
+                .map(element -> element * 10)
+                .retryWhen(element -> {
+                    return element.take(10).delay(2, TimeUnit.SECONDS);
+                })
+                .subscribe(System.out::println);
+
+        try {
+            Thread.sleep(10000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 
     private static void print(Integer element) {
