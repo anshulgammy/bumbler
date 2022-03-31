8209c89a9e1ea920427fbf3a10768dd4d850a247
anshulgammy <anshulgammy@gmail.com>
2022-03-31 15:14:13 +0530

    All questions solved in StreamApiTestByAnshul

--- usecases/java/stream-api-exercises/src/test/java/space/gavinklfong/demo/streamapi/StreamApiTestByAnshul.java
+++ usecases/java/stream-api-exercises/src/test/java/space/gavinklfong/demo/streamapi/StreamApiTestByAnshul.java
@@ -1,13 +1,19 @@
 package space.gavinklfong.demo.streamapi;
 
 import java.time.LocalDate;
+import java.util.Comparator;
+import java.util.DoubleSummaryStatistics;
 import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import space.gavinklfong.demo.streamapi.models.Customer;
 import space.gavinklfong.demo.streamapi.models.Order;
 import space.gavinklfong.demo.streamapi.models.Product;
 import space.gavinklfong.demo.streamapi.repos.OrderRepo;
@@ -166,7 +172,107 @@ public class StreamApiTestByAnshul {
         .sum();
 
     log.info(String.valueOf(sum));
-
   }
+
+  /**
+   * Calculate order average payment placed on 15-Mar-2021
+   */
+  @Test
+  @DisplayName("Exercise 9")
+  public void exercise_9() {
+    double averagePayment = orderRepo.findAll().stream()
+        .filter(order -> order.getOrderDate().compareTo(LocalDate.of(2021, 3, 15)) == 0)
+        .flatMap(order -> order.getProducts().stream())
+        .mapToDouble(product -> product.getPrice())
+        .average().getAsDouble();
+
+    log.info(String.valueOf(averagePayment));
+  }
+
+  /**
+   * Obtain a collection of statistic figures (i.e. sum, average, max, min, count) for all products
+   * of category “Books”
+   */
+  @Test
+  @DisplayName("Exercise 10")
+  public void exercise_10() {
+    DoubleSummaryStatistics productsStatistics = productRepo.findAll().stream()
+        .filter(product -> product.getCategory().equalsIgnoreCase("Books"))
+        .mapToDouble(product -> product.getPrice())
+        .summaryStatistics();
+
+    log.info("Sum: " + String.valueOf(productsStatistics.getSum()));
+    log.info("Average: " + String.valueOf(productsStatistics.getAverage()));
+    log.info("Max: " + String.valueOf(productsStatistics.getMax()));
+    log.info("Min: " + String.valueOf(productsStatistics.getMin()));
+    log.info("count: " + String.valueOf(productsStatistics.getCount()));
+  }
+
+  /**
+   * Obtain a data map with order id and order’s product count
+   */
+  @Test
+  @DisplayName("Exercise 11")
+  public void exercise_11() {
+    Map<Long, Integer> outputMap = orderRepo.findAll().stream()
+        .collect(Collectors.toMap(order -> order.getId(),
+            order -> order.getProducts().size()));
+
+    log.info(String.valueOf(outputMap));
+  }
+
+  /**
+   * Produce a data map with order records grouped by customer
+   */
+  @Test
+  @DisplayName("Exercise 12")
+  public void exercise_12() {
+    Map<Customer, List<Order>> dataMap =
+        orderRepo.findAll().stream()
+            .collect(Collectors.groupingBy(order -> order.getCustomer()));
+
+    log.info(String.valueOf(dataMap));
+  }
+
+  /**
+   * Produce a data map with order record and product price total sum
+   */
+  @Test
+  @DisplayName("Exercise 13")
+  public void exercise_13() {
+    Map<Order, Double> dataMap =
+        orderRepo.findAll().stream()
+            .collect(Collectors.toMap(Function.identity(),
+                order -> order.getProducts().stream().mapToDouble(Product::getPrice).sum()));
+
+    log.info(String.valueOf(dataMap));
+  }
+
+  /**
+   * Obtain a data map with list of product name by category
+   */
+  @Test
+  @DisplayName("Exercise 14")
+  public void exercise_14() {
+    Map<String, List<String>> dataMap = productRepo.findAll().stream()
+        .collect(Collectors.groupingBy(Product::getCategory,
+            Collectors.mapping(Product::getName, Collectors.toList())));
+
+    log.info(String.valueOf(dataMap));
+  }
+
+  /**
+   * Get the most expensive product by category
+   */
+  @Test
+  @DisplayName("Exercise 15")
+  public void exercise_15() {
+    Map<String, Optional<Product>> dataMap = productRepo.findAll().stream()
+        .collect(Collectors.groupingBy(Product::getCategory, Collectors.maxBy(
+            Comparator.comparing(Product::getPrice))));
+
+    log.info(String.valueOf(dataMap));
+  }
+
 
 }
