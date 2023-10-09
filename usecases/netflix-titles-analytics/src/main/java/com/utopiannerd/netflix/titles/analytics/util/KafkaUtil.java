4e0171dabda8e9577d6b50b522e0420baa9ae243
Anshul Gautam <anshulgammy@gmail.com>
2023-10-09 23:19:37 +0530

    Added more comments

--- usecases/netflix-titles-analytics/src/main/java/com/utopiannerd/netflix/titles/analytics/util/KafkaUtil.java
+++ usecases/netflix-titles-analytics/src/main/java/com/utopiannerd/netflix/titles/analytics/util/KafkaUtil.java
@@ -39,18 +39,34 @@ public final class KafkaUtil {
         ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
     kafkaConfigurationMap.put(
         ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+    // Give a friendly name to our producer. This will be helpful in debugging purposes.
     kafkaConfigurationMap.put(ProducerConfig.CLIENT_ID_CONFIG, "raw-data-producer");
 
-    // Configuration related to creation of safe Producer
+    // Configuration related to creation of safe Producer:
+
+    // Enable idempotence in the topic. Idempotence refers to be exactly once semantics.
     kafkaConfigurationMap.put(ProducerConfig.ENABLE_IDEMPOTENCE_CONFIG, true);
+    // Broker need to acknowledge receiving the message to producer before it is considered a
+    // successful write.
     kafkaConfigurationMap.put(ProducerConfig.ACKS_CONFIG, ACKS_CONFIG);
+    // Configuring the retry times for a message push in case there is a failure. determines how
+    // many times the producer will attempt to send a message before marking it as failed.
     kafkaConfigurationMap.put(ProducerConfig.RETRIES_CONFIG, RETRIES_CONFIG);
+    // This setting basically controls how many requests can be made in parallel to any partition,
+    // default is 5.
+    // This should be set to 1 in case we want strict ordering.
     kafkaConfigurationMap.put(
         ProducerConfig.MAX_IN_FLIGHT_REQUESTS_PER_CONNECTION, MAX_IN_FLIGHT_CONN);
 
-    // Additional configuration for high throughput.
+    // Additional configuration for high throughput:
+
+    // Compress the message using this compression type.
     kafkaConfigurationMap.put(ProducerConfig.COMPRESSION_TYPE_CONFIG, COMPRESSION_TYPE);
+    // Wait/Linger for this much of time before pushing the batch of messages by producer to broker.
+    // Messages received in this timeframe will be added to the batch, and post the linger.ms has
+    // elapsed the batch of messages will be pushed to broker.
     kafkaConfigurationMap.put(ProducerConfig.LINGER_MS_CONFIG, LINGER_CONFIG);
+    // Define the batch size for the producer.
     kafkaConfigurationMap.put(ProducerConfig.BATCH_SIZE_CONFIG, BATCH_SIZE);
 
     return kafkaConfigurationMap;
@@ -67,6 +83,9 @@ public final class KafkaUtil {
         ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
     kafkaConfigurationMap.put(
         ConsumerConfig.GROUP_ID_CONFIG, "netflix-titles-analytics-raw-data-group");
+    // Define the behavior of the consumer when there is no committed position (which would be the
+    // case when the group is first initialized) or when an offset is out of range. You can choose
+    // either to reset the position to the “earliest” offset or the “latest” offset (the default)
     kafkaConfigurationMap.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
 
     return kafkaConfigurationMap;
